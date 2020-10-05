f67b4a339e935afde77614759ce32a4fc23df2a9
tusu18 <42227745+tusu18@users.noreply.github.com>
2020-10-06 01:57:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ os.environ["NUMEXPR_MAX_THREADS"]="16"
 os.environ["NUMEXPR_NUM_THREADS"]="16"
 st.set_option('deprecation.showfileUploaderEncoding', False)
 
-@st.cache(persist=True)
+
 def load_data(file):
     file=cv2.imread(file)
     file=cv2.resize(file,(120,120),interpolation=cv2.INTER_CUBIC)
@@ -29,10 +29,9 @@ st.markdown("This application is made for image Blurr Detection")
 st.markdown("![Alt Text](https://cnet1.cbsistatic.com/img/vIjS19RgmQrE_noolcMz-WkrANs=/1092x614/2019/05/31/a01d0905-3b69-45d8-92e1-c0a26dc7dec5/motion-blur.jpg)")
 st.sidebar.title("A better Image blurr detection")
 st.sidebar.markdown("Select from folder image")
-if not st.sidebar.checkbox("Hide",True):
-    st.markdown("Upload Image let me tell yor skill")
-    file=st.file_uploader("Please Upload an File",type=["jpg","jpeg","png"])
-    data=load_data(file)   
+st.markdown("Upload Image let me tell yor skill")
+file=st.file_uploader("Please Upload an File",type=["jpg","jpeg","png"])
+data=load_data(file)   
 if file is None:
     st.text("Add an Image so i can give some inference")
 else:
@@ -40,7 +39,7 @@ else:
     st.image(image,use_column_width=True)
     prd=predict(data)
     if prd[0]==-1:
-       st.write("Good works its a clear pic")
+       st.write("Good work its a clear pic")
     else:
        st.write("Man hold your hands its blurr!!")        
 
