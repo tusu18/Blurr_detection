a7541509f6bdfc5cb0f79b4ae0f46741bb33f99d
tusu18 <42227745+tusu18@users.noreply.github.com>
2020-10-06 04:49:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,24 +6,39 @@ import matplotlib.pyplot as plt
 from joblib import load
 import cv2
 from PIL import Image, ImageOps
+import pickle
 os.environ["NUMEXPR_MAX_THREADS"]="16"
 os.environ["NUMEXPR_NUM_THREADS"]="16"
 st.set_option('deprecation.showfileUploaderEncoding', False)
 
-@st.cache(suppress_st_warning=True)
+def laplacesobel(gray):
+    lvar=cv2.Laplacian(gray,cv2.CV_64F).var()
+    lmax=np.amax(cv2.Laplacian(gray,cv2.CV_64F))
+    svarX=cv2.Sobel(gray,cv2.CV_64F,1,0,ksize=5).var()
+    smaxX=np.amax(cv2.Sobel(gray,cv2.CV_64F,1,0,ksize=5))
+    svarY=cv2.Sobel(gray,cv2.CV_64F,0,1,ksize=5).var()
+    smaxY=np.amax(cv2.Sobel(gray,cv2.CV_64F,0,1,ksize=5))
+    scarX=cv2.Scharr(gray,cv2.CV_64F,0,1).var()
+    scmaxX=np.amax(cv2.Scharr(gray,cv2.CV_64F,0,1))
+    scarY=cv2.Scharr(gray,cv2.CV_64F,1,0).var()
+    scmaxY=np.amax(cv2.Scharr(gray,cv2.CV_64F,1,0))
+
+
+    return lvar,lmax,svarX,smaxX,svarY,smaxY,scarX,scmaxX,scarY,scmaxY
+@st.cache
 def get_user_input(image_data,model):    
     size=(120,120)
     image = ImageOps.fit(image_data,size, Image.ANTIALIAS)
     image = np.asarray(image)
+    image = cv2.imread(image_data)
     img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     gray = cv2.resize(img, dsize=(120, 120),    interpolation=cv2.INTER_CUBIC)
-    svarX=cv2.Sobel(gray,cv2.CV_64F,1,0,ksize=5).var()
-    smaxY=np.amax(cv2.Sobel(gray,cv2.CV_64F,0,1,ksize=5))
-    data=np.array([smaxY,svarX])
+    lvar,lmax,svarX,smaxX,svarY,smaxY,scarX,scmaxX,scarY,scmaxY=laplacesobel(gray)
+    data=np.array([lvar,lmax,svarX,smaxX,svarY,smaxY,scarX,scmaxX,scarY,scmaxY])
     data=data.reshape(1,-1)
     predict=model.predict(data)
     return predict
-model=load("blurr_class.joblib")
+model=joblib.load("Blurr_Model.pckl")
 st.title("BLURR IMAGE DETECTION MODEL")
 st.markdown("This application is made for image Blurr Detection")
 st.markdown("![Alt Text](https://cnet1.cbsistatic.com/img/vIjS19RgmQrE_noolcMz-WkrANs=/1092x614/2019/05/31/a01d0905-3b69-45d8-92e1-c0a26dc7dec5/motion-blur.jpg)")
